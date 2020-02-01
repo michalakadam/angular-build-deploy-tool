88e9a663d935783a4a4f425ab9167b386afaadb1
Adam Michalak <adam.michalak.dev@gmail.com>
2020-02-01 16:24:57 +0100

    update config variables' names

--- git_push_server_update.sh
+++ git_push_server_update.sh
@@ -9,8 +9,8 @@ cd $source_location_locally
 
 #Push changes to remote repo
 echo "Pushing local changes to $remote_repo_local_name"
-git push origin master
+git push $remote_repo_local_name master
 
 #Inform server about changes being pushed
-echo "Establishing connection with $address"
-ssh $user@$address time_to_pull $source_location_remotely $remote_repo_server_name $built_project_location
+echo "Establishing connection with $ip_address"
+ssh $su_name@$ip_address $source_location_remotely/pull_and_build.sh $source_location_remotely $remote_repo_server_name $built_project_location
